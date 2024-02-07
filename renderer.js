78382e22a6c5e4363af37230cdfa77e487eaa71e
Victor Torres <vtorremo@emeal.nttdata.com>
2024-02-07 13:08:30 -0500

    Demo inicial de electron

---
+++ renderer.js
@@ -0,0 +1,2 @@
+const information = document.getElementById("info");
+information.innerText = `Esta aplicación está usando Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
